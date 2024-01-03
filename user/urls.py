3eea3cf2b33b4d833e8ebb98280b4602e2204987
Tushar Sahani <tusharsahani000@gmail.com>
2024-01-03 18:05:52 +0530

    Login

--- user/urls.py
+++ user/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('',views.index,name=""),
     path('register',views.register,name="register"),
     path('login',views.login,name="login"),
-    path('dashboard',views.dashboard,name="dashboard"),
+    path('dashboard/<str:username>/',views.dashboard,name="dashboard"),
+    path('logout',views.logout,name="logout"),
     
 ]
--- user/views.py
+++ user/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render,redirect
-from . forms import CreateUserForm
+from . forms import CreateUserForm, LoginForm
+
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate,login,logout
+
 # Create your views here.
 
 def index(request):
@@ -14,13 +19,39 @@ def register(request):
 
         if form.is_valid():
             form.save()
+
             return redirect("login")
+        
     context = {'registerform':form}   
     return render(request,'user/register.html',context=context)
 
 
 def login(request):
-    return render(request,'user/login.html')
 
-def dashboard(request):
-    return render(request,'user/dashboard.html')
+    form = LoginForm()
+
+    if request.method == 'POST':
+        form = LoginForm(request, data = request.POST)
+
+        if form.is_valid():
+
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username,password=password)
+
+            if user is not None:
+                auth.login(request,user)
+                return redirect('dashboard',username=username)
+    context = {'loginform':form}
+    return render(request,'user/login.html',context=context)
+
+@login_required(login_url="login")
+def dashboard(request,username):
+    context = {"username":username}
+    return render(request,'user/dashboard.html',context=context)
+
+def logout(request):
+    auth.logout(request)
+    
+    return redirect("login")
